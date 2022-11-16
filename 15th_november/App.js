6ebc86a66ba8daa0100417e569c7f9472b6fd283
Pushkar Kaushik <36399726+Pushkarkaushik1997@users.noreply.github.com>
2022-11-16 22:38:20 +0530

    15_nov work

---
+++ 15th_november/App.js
@@ -0,0 +1,5 @@
+const heading=React.createElement("h1",{},"Hello world"); // properties we cam add like {id:"title"}
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root)
+root.render(heading)
