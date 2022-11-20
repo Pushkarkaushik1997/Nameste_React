73a7bc0a5964222faf4989dcfad3a1fd7394c91b
Pushkarkaushik1997 <pushkarkaushik1997@gmail.com>
2022-11-21 00:45:48 +0530

    17th_nov

---
+++ 17th_nov/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading=React.createElement("h1",{},"Hello world"); // properties we cam add like {id:"title"}
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root)
+root.render(heading)
